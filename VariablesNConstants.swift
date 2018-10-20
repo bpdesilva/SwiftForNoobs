c5579405e0bc21a2324fcc04f88bc5d7d6e6682f
Buwaneka De Silva <buwanekapasan@gmail.com>
2018-10-20 14:59:12 +0530

    Create VariablesNConstants.swift

---
+++ VariablesNConstants.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var someNumber = 1 // Variable with var keyword
+let pi = 3.14 // Constant values with the let keyword
+
+someNumber = 4 //value changes
+//pi = 3 //Constant
+
+var val_1  = 2 , val_2 = 4 , val_3 = 6 //multiple variable declarations
