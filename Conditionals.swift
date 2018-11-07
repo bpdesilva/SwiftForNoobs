ab1c4493ceff2157468e594049cfa78bd2cf1b8e
bpdesilva <buwanekapasan@gmail.com>
2018-11-07 09:31:34 +0530

    New Files

---
+++ Conditionals.swift
@@ -0,0 +1,22 @@
+//Comparisons
+var higherValue = 200
+var lowerValue = 100
+
+higherValue == lowerValue // equal to
+higherValue != lowerValue // not equal
+higherValue <= lowerValue // less than or equal to lower value
+higherValue >= lowerValue // greater than or equal to lower value
+higherValue > lowerValue // greater than lower value
+higherValue < lowerValue // less than lower value
+
+//Ternary
+var points = higherValue < 50 ? 40 : 20
+
+//Logical
+var isJogging = true
+var isJumping = !isJogging
+isJogging && isJumping // AND
+isJogging || isJumping //OR
+
+
+
---
+++ Operators.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//Arithmetic
+var arithmeticOps = 10 + 5 - 3 / 2 * 12
+
+//Modulus
+var modulusOf = 7%2
+
+//Compound
+var someValue = 10
+someValue /= 2
+someValue = someValue / 2
+
